64298e6130950043f0db722ab9b3b7c9398f1b3a
mushruf12 <mushrufs@gmail.com>
2022-10-17 16:56:13 +0530

    RestController

---
+++ CRUD/src/main/java/com/example/kpi/database/Student.java
@@ -0,0 +1,14 @@
+package com.example.kpi.database;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
+public class Student {
+	@GetMapping("/student")
+	public void student() {
+
+	}
+	
+}
